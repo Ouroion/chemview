e29566ae1efa244fc1fb5ed869e60ac5ebbd4583
Ouroion <maealex6@gmail.com>
2025-12-09 01:40:47 -0500

    Can now get multiple nodes from json response for a given chemical that a user inputs.

--- src/main/java/org/chemview/api/pubChemEndpoint.java
+++ src/main/java/org/chemview/api/pubChemEndpoint.java
@@ -1,5 +1,6 @@
 package org.chemview.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
@@ -11,18 +12,20 @@ public class pubChemEndpoint {
     private static final String pcURL = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static String getChemicalFormula(String chemical) throws Exception {
+    public static JsonNode getChemicalFormula() throws Exception {
         String response;
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter chemical name (i.e. \"aspirin\")");
 
         String input = scan.nextLine().trim();
-        String pcURLParams = pcURL + input + "/property/MolecularFormula/TXT";
+        String pcURLParams = pcURL + input + "/JSON";
+        //String pcURLParams = pcURL + input + "/property/MolecularFormula/TXT";
 
         URL url = new URL(pcURLParams);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        try{
+
+        /*try{
             // Read directly from the connection's input stream
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(conn.getInputStream())
@@ -37,9 +40,39 @@ public class pubChemEndpoint {
             reader.close();
         } finally {
             conn.disconnect();
-        }
+        }*/
 
-        return response;
+        return readResponse(conn);
+       // return response;
     }
 
+    public static JsonNode readResponse(HttpURLConnection conn) throws Exception {
+        int responseCode = conn.getResponseCode();
+        System.out.println("Response code: " + responseCode);
+        System.out.println("Response message: " + conn.getResponseMessage());
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return objectMapper.readTree(response.toString());
+            }
+        } else {
+            // Read error response
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getErrorStream()))) {
+                StringBuilder errorResponse = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    errorResponse.append(inputLine);
+                }
+                System.err.println("Error response body: " + errorResponse.toString());
+            } catch (Exception e) {
+                System.err.println("Could not read error response: " + e.getMessage());
+            }
+            throw new Exception("API request failed with response code: " + responseCode);
+        }
+    }
 }
